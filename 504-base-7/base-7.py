35ed7cdab5600fd4ce637b042f9ae26086be9af7
rajadityapal05 <rajadityapal2006@gmail.com>
2026-09-02 18:52:34 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (31.12%) - LeetSync

---
+++ 504-base-7/base-7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        if num == 0:
+            return "0"
+
+        sign = "-" if num < 0 else ""
+        num = abs(num)
+
+        ans = ""
+
+        while num > 0:
+            ans += str(num % 7)
+            num //= 7
+
+        return sign + ans[::-1]
