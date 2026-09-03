fe2ba6ae4f797f6d8a40cdbf7f83155f1e0b7c2e
rajadityapal05 <rajadityapal2006@gmail.com>
2026-09-03 15:40:53 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (81.77%) - LeetSync

---
+++ 365-water-and-jug-problem/water-and-jug-problem.py
@@ -0,0 +1,6 @@
+class Solution:
+    def canMeasureWater(self, x: int, y: int, target: int) -> bool:
+        if target > x + y:
+            return False
+
+        return target % math.gcd(x, y) == 0
