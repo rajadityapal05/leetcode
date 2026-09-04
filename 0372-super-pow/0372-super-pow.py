a520cf2af81feca929476c94259dc1ac73677866
rajadityapal05 <rajadityapal2006@gmail.com>
2026-09-04 13:13:02 +0530

    Time: 8 ms (84.76%), Space: 19.2 MB (77.14%) - LeetHub

---
+++ 0372-super-pow/0372-super-pow.py
@@ -0,0 +1,9 @@
+class Solution:
+    def superPow(self, a: int, b: List[int]) -> int:
+        result = 1
+        a %= 1337
+
+        for digit in b:
+            result = (pow(result, 10, 1337) * pow(a, digit, 1337)) % 1337
+
+        return result
