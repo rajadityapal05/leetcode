6f00fb238ceed3b896ff409855cd18a71597b33f
rajadityapal05 <rajadityapal2006@gmail.com>
2026-09-20 17:06:39 +0530

    Time: 22 ms (82.91%) | Memory: 19.7 MB (68.35%) - LeetSync

---
+++ 1543-simplified-fractions/simplified-fractions.py
@@ -0,0 +1,12 @@
+from math import gcd
+
+class Solution:
+    def simplifiedFractions(self, n: int) -> list[str]:
+        result = []
+
+        for denominator in range(2, n + 1):
+            for numerator in range(1, denominator):
+                if gcd(numerator, denominator) == 1:
+                    result.append(f"{numerator}/{denominator}")
+
+        return result
